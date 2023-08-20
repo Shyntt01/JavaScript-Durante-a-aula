4b1b6e57c440a736a72ef8d75a4a320edaeddce0
felipe <felipe.impt@hotmail.com>
2023-08-20 18:14:29 -0300

    Aula 6. Projeto de calculadora finalizado

---
+++ Aula 6 - Aprofundando em funções/script.js
@@ -0,0 +1,17 @@
+// tipos de função
+
+//declarativos
+/*function funcao() {
+    console.log('Sou uma msg de função declarativa')
+}
+funcao()*/
+
+/*var funcao = function() {
+    console.log('Sou uma mensagem de função de expressão')
+}
+funcao()*/
+
+/*var funcao = () => {
+    console.log('Sou uma arrow function')
+}
+funcao()*/
